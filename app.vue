62fead9b42b5262f4e96ba4912460c7065b146b1
nac-39 <83295760+nac-39@users.noreply.github.com>
2022-12-26 07:08:34 +0000

    init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+console.log("hello world")
+</script>
+
+<template>
+  <div>
+    <HellloWorld />
+  </div>
+</template>
+
---
+++ components/HellloWorld.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>Hello world!</h1>
+    <p>this is nu-wiki!</p>
+</template>
