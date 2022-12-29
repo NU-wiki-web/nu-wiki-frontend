0460d616b653fbf9fd5c1242e82c8bb99f2be5a8
nac-39 <83295760+nac-39@users.noreply.github.com>
2022-12-30 00:53:31 +0900

    [add] vuetifyとtailwindcss

--- components/HellloWorld.vue
+++ components/HellloWorld.vue
@@ -1,4 +1,34 @@
 <template>
-    <h1>Hello world!</h1>
-    <p>this is nu-wiki!</p>
+  <h1>Hello world!</h1>
+  <p>this is nu-wiki!</p>
+
+  <v-btn class="p-10" @click="onClick">vuetifyです！！</v-btn>
+  <v-btn class="p-10" @click="onClickDelete">hogeを消す</v-btn>
+  <p v-for="t in text">
+    <span class="m-2 text-yellow-500">{{ t }}</span>
+  </p>
 </template>
+
+<script lang="ts">
+import { text } from "stream/consumers";
+import { defineComponent, ref, Ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    const text: Ref<String[]> = ref([]);
+    const functions = {
+      onClick() {
+        text.value = [...text.value, "hoge"];
+      },
+      onClickDelete(){
+        text.value = []
+      }
+    };
+
+    return {
+      text,
+      ...functions,
+    };
+  },
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  css: ["vuetify/lib/styles/main.sass", "~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  build: {
+    transpile: ["vuetify"],
+  },
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
+    },
+  },
+});
