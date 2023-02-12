adb49dc4b28cfd2373c47ba405751c5080cfbd65
nac-39 <83295760+nac-39@users.noreply.github.com>
2023-02-12 14:56:18 +0900

    wip

---
+++ api/file/_file_id@string/index.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import type { ReadStream } from 'fs'
+
+export type Methods = {
+  put: {
+    status: 200
+    /** pdfファイル */
+    reqBody: (File | ReadStream)
+  }
+
+  delete: {
+    status: 200
+    /** pdfファイル */
+    reqBody: (File | ReadStream)
+  }
+}
---
+++ components/pages/upload.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { useFetch, useClient } from "~/util/api/useApi";
+const { res, error } = await useFetch(() => {
+  const client = useClient();
+  return client.get();
+});
+</script>
+<template>{{ res }}</template>
