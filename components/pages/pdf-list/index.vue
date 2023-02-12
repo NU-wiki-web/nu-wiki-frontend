556623cb72ae6f0b008f069c6a524e0ff67ae90d
yutteee <yusaku_nakamura_aa@icloud.com>
2023-02-12 18:35:14 +0900

    :+1: ダミーデータからコンポーネント間の値渡しできた #3

--- components/pages/pdf-list/index.vue
+++ components/pages/pdf-list/index.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    <UiPdfListItem></UiPdfListItem>
+    <v-list v-for="pdf in samplePdfs" :key="pdf.file_id" border>
+      <UiPdfListItem :file_id="pdf.file_id" :name="pdf.name"></UiPdfListItem>
+    </v-list>
   </div>
 </template>
+
+<script setup lang="ts">
+import { reactive } from "vue";
+
+const samplePdfs = reactive([
+  {
+    file_id: 1,
+    name: "有機化学3",
+  },
+  {
+    file_id: 2,
+    name: "実験安全学",
+  },
+]);
+</script>
