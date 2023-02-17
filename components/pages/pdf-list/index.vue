82588a00002750e08b41f3130d9311a9d8665b57
yutteee <yusaku_nakamura_aa@icloud.com>
2023-02-17 23:34:51 +0900

    :+1: リストにクリックイベントを追加 #3

--- components/pages/pdf-list/index.vue
+++ components/pages/pdf-list/index.vue
@@ -3,14 +3,24 @@
     <h1>PDF一覧</h1>
     <div v-if="error">PDFの取得に失敗しました。もう一度お試しください。</div>
     <v-list v-for="pdf in pdfs" :key="pdf.file_id" border>
-      <UiPdfListItem :file_id="pdf.file_id" :name="pdf.name"></UiPdfListItem>
+      <UiPdfListItem
+        :file_id="pdf.file_id"
+        :name="pdf.name"
+        @click="openPdfDetail(pdf.file_id)"
+      ></UiPdfListItem>
     </v-list>
   </div>
 </template>
 
 <script setup lang="ts">
-// 参考: https://reffect.co.jp/vue/nuxt3#Data_Fetching
 const { data: pdfs, error } = useFetch(
   "https://9qlhq2c3rf.execute-api.ap-northeast-1.amazonaws.com/dev/files"
 );
+
+console.error(error);
+
+function openPdfDetail(file_id: Number) {
+  // TODO: file_idからpdfの詳細データをバックから受け取る
+  console.log(file_id);
+}
 </script>
