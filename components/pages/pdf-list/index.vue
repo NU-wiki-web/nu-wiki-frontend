0a4ca98f25f7fbaf2eb7dd21ef30b078f16a8671
yutteee <yusaku_nakamura_aa@icloud.com>
2023-02-17 23:24:12 +0900

    :add: モックの/filesを作成してpdf一覧を取得 #3

--- components/pages/pdf-list/index.vue
+++ components/pages/pdf-list/index.vue
@@ -1,22 +1,16 @@
 <template>
   <div>
-    <v-list v-for="pdf in samplePdfs" :key="pdf.file_id" border>
+    <h1>PDF一覧</h1>
+    <div v-if="error">PDFの取得に失敗しました。もう一度お試しください。</div>
+    <v-list v-for="pdf in pdfs" :key="pdf.file_id" border>
       <UiPdfListItem :file_id="pdf.file_id" :name="pdf.name"></UiPdfListItem>
     </v-list>
   </div>
 </template>
 
 <script setup lang="ts">
-import { reactive } from "vue";
-
-const samplePdfs = reactive([
-  {
-    file_id: 1,
-    name: "有機化学3",
-  },
-  {
-    file_id: 2,
-    name: "実験安全学",
-  },
-]);
+// 参考: https://reffect.co.jp/vue/nuxt3#Data_Fetching
+const { data: pdfs, error } = useFetch(
+  "https://9qlhq2c3rf.execute-api.ap-northeast-1.amazonaws.com/dev/files"
+);
 </script>
