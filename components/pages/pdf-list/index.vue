1fbe6cf920c589b637be9fa88c44929de1e66edd
kentakom1213 <kentakom1213@gmail.com>
2023-02-25 19:16:54 +0900

    [add] (ERROR) pdfの詳細をモックサーバから取得

--- components/pages/pdf-list/index.vue
+++ components/pages/pdf-list/index.vue
@@ -14,13 +14,19 @@
 
 <script setup lang="ts">
 const { data: pdfs, error } = useFetch(
-  "https://9qlhq2c3rf.execute-api.ap-northeast-1.amazonaws.com/dev/files"
+  "https://8d0okqs9rh.execute-api.ap-northeast-1.amazonaws.com/dev/files"
 );
 
 console.error(error);
 
 function openPdfDetail(file_id: Number) {
   // TODO: file_idからpdfの詳細データをバックから受け取る
-  console.log(file_id);
+  const { data: pdf_detail, error } = useFetch(
+    "https://qsdpsf0myh.execute-api.ap-northeast-1.amazonaws.com/dev/detail/1",
+    {
+      headers: { 'Access-Control-Allow-Origin': '*' /* CORSエラー対策 */ },
+    }
+  );
+  console.log(pdf_detail);
 }
 </script>
