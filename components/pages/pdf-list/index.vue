d883887746a45f97476b1f9c87b3eb05a06f1326
kentakom1213 <kentakom1213@gmail.com>
2023-02-26 18:19:18 +0900

    [add] モーダルの表示/非表示

--- components/pages/pdf-list/index.vue
+++ components/pages/pdf-list/index.vue
@@ -9,24 +9,41 @@
         @click="openPdfDetail(pdf.file_id)"
       ></UiPdfListItem>
     </v-list>
+    <pdfDetailModal v-if="showDetail" @close="closePdfDetail">
+      <!-- タイトルのスロットコンテンツ -->
+      <h3>{{ pdfDetailData.value["name"] }}</h3>
+      {{ pdfDetailData.value["file_id"] }}
+    </pdfDetailModal>
   </div>
 </template>
 
 <script setup lang="ts">
+import pdfDetailModal from "./pdfDetailModal.vue";
+
 const { data: pdfs, error } = useFetch(
   "https://8d0okqs9rh.execute-api.ap-northeast-1.amazonaws.com/dev/files"
 );
 
+console.log(pdfs);
 console.error(error);
 
+/* pdfの詳細表示 */
+const showDetail = ref(true);  // モーダルを表示するか
+const pdfDetailData = ref({});  // pdfの詳細情報
+
 function openPdfDetail(file_id: Number) {
+  showDetail.value = true;
+
   // TODO: file_idからpdfの詳細データをバックから受け取る
   const { data: pdf_detail, error } = useFetch(
-    "https://qsdpsf0myh.execute-api.ap-northeast-1.amazonaws.com/dev/detail/1",
-    {
-      headers: { 'Access-Control-Allow-Origin': '*' /* CORSエラー対策 */ },
-    }
+    `https://qsdpsf0myh.execute-api.ap-northeast-1.amazonaws.com/dev/detail/${file_id}`
   );
   console.log(pdf_detail);
+  
+  pdfDetailData.value = pdf_detail;
+}
+
+function closePdfDetail() {
+  showDetail.value = false;
 }
 </script>
