8a2dbe54f5723bd9d7620856e19c3aaf5aa5b07c
kentakom1213 <kentakom1213@gmail.com>
2023-03-02 03:50:41 +0900

    [add] 情報がないときの場合わけを追加

--- components/pages/pdf-list/index.vue
+++ components/pages/pdf-list/index.vue
@@ -9,10 +9,24 @@
         @click="openPdfDetail(pdf.file_id)"
       ></UiPdfListItem>
     </v-list>
+    <!-- 詳細情報用のモーダル -->
     <pdfDetailModal v-if="showDetail" @close="closePdfDetail">
       <!-- タイトルのスロットコンテンツ -->
-      <h3>{{ pdfDetailData.value["name"] }}</h3>
-      {{ pdfDetailData.value["file_id"] }}
+      <template v-slot:file_name>
+        <div v-if="pdfDetailError.value">情報がありません</div>
+        <div v-else>{{ pdfDetailData.value.name }}</div>
+      </template>
+      <!-- urlとリンク -->
+      <template v-slot:url>
+        <div
+          v-if="!pdfDetailError.value"
+          class="mr-2 mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          <button @click="openExternalLink(pdfDetailData.value.url)">
+            ファイルを開く
+          </button>
+        </div>
+      </template>
     </pdfDetailModal>
   </div>
 </template>
@@ -21,29 +35,36 @@
 import pdfDetailModal from "./pdfDetailModal.vue";
 
 const { data: pdfs, error } = useFetch(
-  "https://8d0okqs9rh.execute-api.ap-northeast-1.amazonaws.com/dev/files"
+  "http://nu-wiki-mock-pdf-detail.shuttleapp.rs/list"
 );
 
 console.log(pdfs);
 console.error(error);
 
 /* pdfの詳細表示 */
-const showDetail = ref(true);  // モーダルを表示するか
-const pdfDetailData = ref({});  // pdfの詳細情報
+const showDetail = ref(false); // モーダルを表示するか
+const pdfDetailData = ref({}); // pdfの詳細情報
+const pdfDetailError = ref(); // pdfの詳細情報取得時のエラー
 
 function openPdfDetail(file_id: Number) {
   showDetail.value = true;
 
   // TODO: file_idからpdfの詳細データをバックから受け取る
-  const { data: pdf_detail, error } = useFetch(
-    `https://qsdpsf0myh.execute-api.ap-northeast-1.amazonaws.com/dev/detail/${file_id}`
+  const { data: pdf_detail, error: pdf_detail_error } = useFetch(
+    `http://nu-wiki-mock-pdf-detail.shuttleapp.rs/detail/${file_id}`
   );
-  console.log(pdf_detail);
-  
+
   pdfDetailData.value = pdf_detail;
+  pdfDetailError.value = pdf_detail_error;
+
+  console.log(pdf_detail_error);
 }
 
 function closePdfDetail() {
   showDetail.value = false;
+}
+
+function openExternalLink(url: string) {
+  window.open(url, "_blank");
 }
 </script>
--- components/pages/pdf-list/pdfDetailModal.vue
+++ components/pages/pdf-list/pdfDetailModal.vue
@@ -2,9 +2,10 @@
   <div class="fixed inset-0 z-10 overflow-y-auto">
     <div
       class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+      @click.self="$emit('close')"
     >
       <div
-        class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-xl"
+        class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-3xl"
       >
         <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
           <div class="sm:flex sm:items-start">
@@ -13,19 +14,15 @@
                 class="text-base font-semibold leading-6 text-gray-900"
                 id="modal-title"
               >
-                <slot />
+                <!-- ファイル名のスロット -->
+                <slot name="file_name"></slot>
               </h3>
             </div>
           </div>
         </div>
         <div class="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-          <button
-            type="button"
-            class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-            @click="$emit('close')"
-          >
-            Close
-          </button>
+          <!-- リンク -->
+          <slot name="url"></slot>
         </div>
       </div>
     </div>
