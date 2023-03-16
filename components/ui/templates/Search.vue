f44541f7005bd16a23536caa7c6324dbf961e0bb
yutteee <yusaku_nakamura_aa@icloud.com>
2023-03-17 01:11:49 +0900

    :+1: セレクトボックスコンポーネントを作成 #15

---
+++ components/ui/templates/Search.vue
@@ -0,0 +1,12 @@
+<template>
+  <UiSelect :label="label" :items="items" :icon="icon" v-model:selected="aaa"></UiSelect>
+  親：{{ aaa }}
+</template>
+
+<script setup lang="ts">
+const label = "aa";
+const items = ["a", "i", "u", "e", "o"];
+const icon = "mdi-vuetify";
+
+const aaa = ref<string | undefined>(undefined);
+</script>
