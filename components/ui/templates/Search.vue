442dea81a67c63802f6337588a1c00d3edde2f69
yutteee <yusaku_nakamura_aa@icloud.com>
2023-03-17 02:15:09 +0900

    :+1: 検索バーを追加 #15

--- components/ui/templates/Search.vue
+++ components/ui/templates/Search.vue
@@ -1,6 +1,25 @@
 <template>
-  <UiSelect :label="label" :items="items" :icon="icon" v-model:selected="aaa"></UiSelect>
-  親：{{ aaa }}
+  <div class="bg-teal-500">
+    <v-container>
+      <v-row>
+        <v-col sm="3">
+          <UiSelect :label="label" :items="items" :icon="icon" v-model:selected="aaa"></UiSelect>
+        </v-col>
+        <v-col sm="3">
+          <UiSelect :label="label" :items="items" :icon="icon" v-model:selected="aaa"></UiSelect>
+        </v-col>
+        <v-col sm="3">
+          <UiSelect :label="label" :items="items" :icon="icon" v-model:selected="aaa"></UiSelect>
+        </v-col>
+        <v-col sm="3">
+          <UiSelect :label="label" :items="items" :icon="icon" v-model:selected="aaa"></UiSelect>
+        </v-col>
+      </v-row>
+      <v-row>
+        <UiSearchBar :handleClick="ccc" v-model:text="bbb"></UiSearchBar>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -9,4 +28,10 @@ const items = ["a", "i", "u", "e", "o"];
 const icon = "mdi-vuetify";
 
 const aaa = ref<string | undefined>(undefined);
+const bbb = ref<string | undefined>(undefined);
+
+const ccc = function () {
+  console.log("aaa");
+  console.log(bbb);
+};
 </script>
