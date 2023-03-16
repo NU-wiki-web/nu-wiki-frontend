c629631f5e12ae2ba207cf47a60d86cc7e2a4128
yutteee <yusaku_nakamura_aa@icloud.com>
2023-03-17 01:11:49 +0900

    :+1: セレクトボックスコンポーネントを作成 #15

---
+++ components/ui/Select.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-select
+    :label="label"
+    :items="items"
+    bg-color="#fff"
+    color="#555"
+    :prepend-inner-icon="icon"
+    v-model="onSelected"
+  ></v-select>
+  子：{{ selected }}
+</template>
+
+<script setup lang="ts">
+interface Props {
+  label: string;
+  items: [string];
+  icon: string;
+  selected: string | undefined;
+}
+const props = defineProps<Props>();
+
+interface Emits {
+  (e: "update:selected", selected: string | undefined): void;
+}
+const emit = defineEmits<Emits>();
+
+const onSelected = computed({
+  get: () => props.selected,
+  set: (value) => {
+    emit("update:selected", value);
+  },
+});
+</script>
