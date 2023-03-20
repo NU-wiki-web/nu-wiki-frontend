3ce3dfe65b0fcedfaa65a30aba5bd386f5d8bf4b
yutteee <yusaku_nakamura_aa@icloud.com>
2023-03-21 02:34:03 +0900

    :+1: 講義一覧のエンドポイントを叩いて表示 #15

--- components/ui/templates/LectureList.vue
+++ components/ui/templates/LectureList.vue
@@ -1,15 +1,17 @@
 <template>
   <v-container>
-    <div>{{ lectures.length }} 件ヒット中 {{ displayLectures.length }} 件表示</div>
-    <v-list v-for="lecture in displayLectures" :key="lecture.id">
-      <UiListItem
-        :id="lecture.id"
-        :title="lecture.name"
-        :subtitle="lecture.teacher_name"
-        @click="handleClick(lecture.id)"
-      ></UiListItem>
-    </v-list>
-    <UiPagination v-model="pageNumber" :length="pageLength"></UiPagination>
+    <div v-if="lectures">
+      <v-list v-for="lecture in displayLectures" :key="lecture.id">
+        <UiListItem
+          :id="lecture.id"
+          :title="lecture.name"
+          :subtitle="lecture.teacher_name"
+          @click="handleClick(lecture.id)"
+        ></UiListItem>
+      </v-list>
+      <UiPagination v-model="pageNumber" :length="pageLength"></UiPagination>
+    </div>
+    <div v-else>読み込み中...</div>
   </v-container>
 </template>
 
@@ -23,12 +25,24 @@ const props = defineProps<Props>();
 
 const pageNumber = ref<number>(1);
 const displayLecturesLength = 10;
-const pageLength = Math.ceil(props.lectures.length / displayLecturesLength);
 
-const displayLectures = computed(() => {
-  return props.lectures.slice(
-    displayLecturesLength * (pageNumber.value - 1),
-    displayLecturesLength * pageNumber.value
-  );
-});
+// 初回読み込み時、lectures.lengthがundefinedとなり、計算ができない
+let pageLength = 1;
+let displayLectures;
+if (props.lectures) {
+  pageLength = Math.ceil(props.lectures.length / displayLecturesLength);
+
+  displayLectures = computed(() => {
+    return props.lectures.slice(
+      displayLecturesLength * (pageNumber.value - 1),
+      displayLecturesLength * pageNumber.value
+    );
+  });
+}
 </script>
+
+<style scoped>
+.v-progress-circular {
+  margin: 1rem;
+}
+</style>
