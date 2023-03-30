592fd43cf384c839b89b2db68bbc62fdbd648b3e
hirakuuuu <m.hiraku1129@gmail.com>
2023-03-30 18:04:58 +0900

    :+1: アイコン付きボタンのコンポーネントを作成 #22

---
+++ components/ui/IconButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-btn
+      width="200"
+      height="50"
+      :prepend-icon="props.buttonIcon"
+      color="#006e4f"
+    >
+      <template v-slot:prepend>
+        <v-icon color="white"></v-icon>
+      </template>
+      <span class="text-white">
+        {{ props.buttonTitle }}
+      </span>
+    </v-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+// プロップス
+interface Props {
+  buttonTitle?: string;
+  buttonIcon?: string;
+}
+
+// プロップスのデフォルト値
+const props = withDefaults(defineProps<Props>(), {
+  buttonTitle: "ファイルを選択",
+  buttonIcon: "mdi-cloud-upload",
+});
+</script>
