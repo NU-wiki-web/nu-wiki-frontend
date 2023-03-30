66084fce22fb89b0ce18d85e2f075ba478eb24e9
nac-39 <nanaco.saitoh39@gmail.com>
2023-03-30 22:53:29 +0900

    :+1: feat: primary colorの設定

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,11 +1,24 @@
-import { createVuetify } from "vuetify";
+import { createVuetify, ThemeDefinition } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+const customLightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    primary: "#006E4F"
+
+  }
+}
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     components,
     directives,
+    theme: {
+      defaultTheme: 'customLightTheme',
+      themes: {
+        customLightTheme,
+      }
+    }
   });
 
   nuxtApp.vueApp.use(vuetify);
