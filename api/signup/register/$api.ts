be73b8476f52fc6b555a7819c9395549d8f9360c
nac-39 <nanaco.saitoh39@gmail.com>
2023-03-31 11:26:34 +0900

    :recycle: generate aspida & fix file-id-> fileid

---
+++ api/signup/register/$api.ts
@@ -0,0 +1,25 @@
+import type { AspidaClient } from 'aspida'
+import type { Methods as Methods0 } from '.'
+
+const api = <T>({ baseURL, fetch }: AspidaClient<T>) => {
+  const prefix = (baseURL === undefined ? 'http://localhost:8080' : baseURL).replace(/\/$/, '')
+  const PATH0 = '/signup/register'
+  const POST = 'POST'
+
+  return {
+    /**
+     * @param option.body - メールアドレス、名前、パスワードを含めたjson
+     */
+    post: (option: { body: Methods0['post']['reqBody'], config?: T | undefined }) =>
+      fetch<void, Methods0['post']['resHeaders'], Methods0['post']['status']>(prefix, PATH0, POST, option).send(),
+    /**
+     * @param option.body - メールアドレス、名前、パスワードを含めたjson
+     */
+    $post: (option: { body: Methods0['post']['reqBody'], config?: T | undefined }) =>
+      fetch<void, Methods0['post']['resHeaders'], Methods0['post']['status']>(prefix, PATH0, POST, option).send().then(r => r.body),
+    $path: () => `${prefix}${PATH0}`
+  }
+}
+
+export type ApiInstance = ReturnType<typeof api>
+export default api
