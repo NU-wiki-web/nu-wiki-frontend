f9738cb3d64c33d0d47f80416355f98c660912d0
hirakuuuu <m.hiraku1129@gmail.com>
2023-04-06 14:38:48 +0900

    :+1: メール認証ページ完成

--- components/pages/signup/auth.vue
+++ components/pages/signup/auth.vue
@@ -7,25 +7,40 @@
   <v-card
     class="mx-auto -mt-[calc(20vh-90px)] mb-12 min-w-[90vw] max-w-[90vw] md:min-w-[370px] md:max-w-[370px] 2xl:min-w-[460px] 2xl:max-w-[460px]"
   >
-    <div class="d-flex pa-0 mt-10 mb-8 justify-center text-3xl text-[#006E4F]">
+    <div class="d-flex pa-0 mt-10 mb-8 justify-center text-2xl text-[#006E4F]">
       メールアドレス認証
     </div>
     <div class="mx-auto w-4/5">
-      <p>***@*.mail.nagoya-u.ac.jp 宛てに、認証用パスワードを送信しました。</p>
-      <p>
+      <div class="my-2 text-base md:text-lg">
+        <b>***@s.mail.nagoya-u.ac.jp</b>
+        宛てに、認証用パスワードを送信しました。
+      </div>
+      <div class="my-2 text-base md:text-lg">
         本文に記載されているパスワードを入力して、本人確認を完了してください。
-      </p>
+      </div>
     </div>
-    <div class="mx-auto w-4/5">
-      <UiOtpInput v-model="data" :fields="6"></UiOtpInput>
-      <div class="pt-5 text-center text-gray-500">{{ data }}</div>
+    <div class="mx-auto my-4 w-4/5">
+      <UiOtpInput v-model="data" :fields="6" />
+      <div class="pt-2 text-center text-gray-500">
+        {{ data ? data : "数字6桁で入力してください" }}
+      </div>
     </div>
 
-    <div class="mt-2 mb-8 text-center">
-      <UiIconButton
-        :buttonTitle="'メール認証'"
-        :buttonIcon="'mdi-email-outline'"
-      />
+    <div class="mt-2 mb-6 text-center">
+      <UiIconButton :buttonTitle="'送信'" :buttonIcon="'mdi-lock'" />
+    </div>
+
+    <div class="mx-auto w-4/5">
+      <v-divider></v-divider>
+    </div>
+
+    <div class="mx-auto w-4/5">
+      <div class="mt-6 mb-4 text-center">
+        <a href="/signup/auth"><u>パスワードを再送する</u></a>
+      </div>
+      <div class="mt-4 mb-8 text-center">
+        <a href="/signup"><u>メールアドレスを変更する</u></a>
+      </div>
     </div>
   </v-card>
 </template>
