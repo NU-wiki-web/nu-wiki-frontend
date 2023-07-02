80065dc1051b20f8906ec8401db162f86e3cbf06
hirakuuuu <m.hiraku1129@gmail.com>
2023-07-02 14:23:41 +0900

    :+1: figmaに沿ってコンポーネント作成、修正 #21

---
+++ components/ui/InputBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex">
+    <label class="block flex items-center pr-4 text-xl text-white">
+      {{ label }}
+    </label>
+    <v-text-field
+      density="compact"
+      variant="solo"
+      :placeholder="placeholder"
+      single-line
+      hide-details
+      v-model="text"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Emits {
+  (e: "click", text: string | undefined): void;
+}
+const emit = defineEmits<Emits>();
+
+defineProps<{
+  label?: string;
+  placeholder?: string;
+}>();
+
+const text = ref<string | undefined>();
+
+const onClick = function () {
+  emit("click", text.value);
+};
+</script>
---
+++ components/ui/SearchBarNew.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="12" sm="10" md="8">
+      <UiInputBar @click="onClick" placeholder="講義名を入力" label="講義名" />
+    </v-col>
+    <v-col cols="2" class="hidden sm:block" />
+  </v-row>
+  <v-row justify="center">
+    <v-col cols="12" sm="10" md="8">
+      <UiInputBar
+        @click="onClick"
+        placeholder="担当教員名を入力"
+        label="教授名"
+      />
+    </v-col>
+    <v-col cols="2" class="flex items-center justify-center sm:justify-end">
+      <v-btn
+        elevation="2"
+        color="#006E4F"
+        class="w-full md:w-1/2"
+        @click="onClick"
+      >
+        <span class="font-normal text-white">検索</span>
+      </v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup lang="ts">
+interface Emits {
+  (e: "click", text: string | undefined): void;
+}
+const emit = defineEmits<Emits>();
+
+const text = ref<string | undefined>();
+
+const onClick = function () {
+  emit("click", text.value);
+};
+</script>
