63b8cbac30d91fe2ac0fe01f48f72c5417984bf6
hirakuuuu <m.hiraku1129@gmail.com>
2023-08-25 15:53:38 +0900

    [add] api client #39

--- api/$api.ts
+++ api/$api.ts
@@ -1,36 +1,36 @@
-import type { AspidaClient, BasicHeaders } from 'aspida'
-import { dataToURLString } from 'aspida'
-import type { Methods as Methods0 } from '.'
-import type { Methods as Methods1 } from './exams/_exam_id/files'
-import type { Methods as Methods2 } from './files'
-import type { Methods as Methods3 } from './files/_fileid@string'
-import type { Methods as Methods4 } from './lectures'
-import type { Methods as Methods5 } from './lectures/_exam_id'
-import type { Methods as Methods6 } from './lectures/search'
-import type { Methods as Methods7 } from './login'
-import type { Methods as Methods8 } from './signup/auth'
-import type { Methods as Methods9 } from './signup/mail'
-import type { Methods as Methods10 } from './signup/register'
+import type { AspidaClient, BasicHeaders } from 'aspida';
+import { dataToURLString } from 'aspida';
+import type { Methods as Methods_by08hd } from '.';
+import type { Methods as Methods_1wezual } from './exams/_exam_id/files';
+import type { Methods as Methods_19v0g2l } from './files';
+import type { Methods as Methods_1eqyz61 } from './files/_fileid@string';
+import type { Methods as Methods_iigf21 } from './lectures';
+import type { Methods as Methods_1ci9g1q } from './lectures/_lecture_id/exams';
+import type { Methods as Methods_1a3o5fu } from './lectures/search';
+import type { Methods as Methods_idk8rz } from './login';
+import type { Methods as Methods_1fdblld } from './signup/auth';
+import type { Methods as Methods_jq4fuo } from './signup/mail';
+import type { Methods as Methods_1izjkiy } from './signup/register';
 
 const api = <T>({ baseURL, fetch }: AspidaClient<T>) => {
-  const prefix = (baseURL === undefined ? 'http://127.0.0.1:8080' : baseURL).replace(/\/$/, '')
-  const PATH0 = '/exams'
-  const PATH1 = '/files'
-  const PATH2 = '/lectures'
-  const PATH3 = '/lectures/search'
-  const PATH4 = '/login'
-  const PATH5 = '/signup/auth'
-  const PATH6 = '/signup/mail'
-  const PATH7 = '/signup/register'
-  const GET = 'GET'
-  const POST = 'POST'
-  const PUT = 'PUT'
-  const DELETE = 'DELETE'
+  const prefix = (baseURL === undefined ? 'http://127.0.0.1:8080' : baseURL).replace(/\/$/, '');
+  const PATH0 = '/exams';
+  const PATH1 = '/files';
+  const PATH2 = '/lectures';
+  const PATH3 = '/lectures/search';
+  const PATH4 = '/login';
+  const PATH5 = '/signup/auth';
+  const PATH6 = '/signup/mail';
+  const PATH7 = '/signup/register';
+  const GET = 'GET';
+  const POST = 'POST';
+  const PUT = 'PUT';
+  const DELETE = 'DELETE';
 
   return {
     exams: {
       _exam_id: (val1: number | string) => {
-        const prefix1 = `${PATH0}/${val1}`
+        const prefix1 = `${PATH0}/${val1}`;
 
         return {
           files: {
@@ -38,131 +38,143 @@ const api = <T>({ baseURL, fetch }: AspidaClient<T>) => {
              * @returns ok
              */
             get: (option?: { config?: T | undefined } | undefined) =>
-              fetch<Methods1['get']['resBody'], BasicHeaders, Methods1['get']['status']>(prefix, `${prefix1}${PATH1}`, GET, option).json(),
+              fetch<Methods_1wezual['get']['resBody'], BasicHeaders, Methods_1wezual['get']['status']>(prefix, `${prefix1}${PATH1}`, GET, option).json(),
             /**
              * @returns ok
              */
             $get: (option?: { config?: T | undefined } | undefined) =>
-              fetch<Methods1['get']['resBody'], BasicHeaders, Methods1['get']['status']>(prefix, `${prefix1}${PATH1}`, GET, option).json().then(r => r.body),
-            $path: () => `${prefix}${prefix1}${PATH1}`
-          }
-        }
-      }
+              fetch<Methods_1wezual['get']['resBody'], BasicHeaders, Methods_1wezual['get']['status']>(prefix, `${prefix1}${PATH1}`, GET, option).json().then(r => r.body),
+            $path: () => `${prefix}${prefix1}${PATH1}`,
+          },
+        };
+      },
     },
     files: {
       _fileid: (val1: string) => {
-        const prefix1 = `${PATH1}/${val1}`
+        const prefix1 = `${PATH1}/${val1}`;
 
         return {
           /**
-           * @param option.body - pdfファイル
+           * @returns OK
            */
-          put: (option: { body: Methods3['put']['reqBody'], config?: T | undefined }) =>
-            fetch<void, BasicHeaders, Methods3['put']['status']>(prefix, prefix1, PUT, option).send(),
+          get: (option?: { config?: T | undefined } | undefined) =>
+            fetch<Methods_1eqyz61['get']['resBody'], BasicHeaders, Methods_1eqyz61['get']['status']>(prefix, prefix1, GET, option).blob(),
+          /**
+           * @returns OK
+           */
+          $get: (option?: { config?: T | undefined } | undefined) =>
+            fetch<Methods_1eqyz61['get']['resBody'], BasicHeaders, Methods_1eqyz61['get']['status']>(prefix, prefix1, GET, option).blob().then(r => r.body),
           /**
            * @param option.body - pdfファイル
            */
-          $put: (option: { body: Methods3['put']['reqBody'], config?: T | undefined }) =>
-            fetch<void, BasicHeaders, Methods3['put']['status']>(prefix, prefix1, PUT, option).send().then(r => r.body),
+          put: (option: { body: Methods_1eqyz61['put']['reqBody'], config?: T | undefined }) =>
+            fetch<void, BasicHeaders, Methods_1eqyz61['put']['status']>(prefix, prefix1, PUT, option).send(),
+          /**
+           * @param option.body - pdfファイル
+           */
+          $put: (option: { body: Methods_1eqyz61['put']['reqBody'], config?: T | undefined }) =>
+            fetch<void, BasicHeaders, Methods_1eqyz61['put']['status']>(prefix, prefix1, PUT, option).send().then(r => r.body),
           delete: (option?: { config?: T | undefined } | undefined) =>
-            fetch<void, BasicHeaders, Methods3['delete']['status']>(prefix, prefix1, DELETE, option).send(),
+            fetch<void, BasicHeaders, Methods_1eqyz61['delete']['status']>(prefix, prefix1, DELETE, option).send(),
           $delete: (option?: { config?: T | undefined } | undefined) =>
-            fetch<void, BasicHeaders, Methods3['delete']['status']>(prefix, prefix1, DELETE, option).send().then(r => r.body),
-          $path: () => `${prefix}${prefix1}`
-        }
+            fetch<void, BasicHeaders, Methods_1eqyz61['delete']['status']>(prefix, prefix1, DELETE, option).send().then(r => r.body),
+          $path: () => `${prefix}${prefix1}`,
+        };
       },
       /**
        * @returns OK
        */
       get: (option?: { config?: T | undefined } | undefined) =>
-        fetch<Methods2['get']['resBody'], BasicHeaders, Methods2['get']['status']>(prefix, PATH1, GET, option).json(),
+        fetch<Methods_19v0g2l['get']['resBody'], BasicHeaders, Methods_19v0g2l['get']['status']>(prefix, PATH1, GET, option).json(),
       /**
        * @returns OK
        */
       $get: (option?: { config?: T | undefined } | undefined) =>
-        fetch<Methods2['get']['resBody'], BasicHeaders, Methods2['get']['status']>(prefix, PATH1, GET, option).json().then(r => r.body),
+        fetch<Methods_19v0g2l['get']['resBody'], BasicHeaders, Methods_19v0g2l['get']['status']>(prefix, PATH1, GET, option).json().then(r => r.body),
       /**
        * @param option.body - 講義と講義のテストを指定して講義資料をアップロードする
        * @returns OK
        */
-      post: (option: { body: Methods2['post']['reqBody'], query: Methods2['post']['query'], config?: T | undefined }) =>
-        fetch<Methods2['post']['resBody'], BasicHeaders, Methods2['post']['status']>(prefix, PATH1, POST, option).json(),
+      post: (option: { body: Methods_19v0g2l['post']['reqBody'], query: Methods_19v0g2l['post']['query'], config?: T | undefined }) =>
+        fetch<Methods_19v0g2l['post']['resBody'], BasicHeaders, Methods_19v0g2l['post']['status']>(prefix, PATH1, POST, option).json(),
       /**
        * @param option.body - 講義と講義のテストを指定して講義資料をアップロードする
        * @returns OK
        */
-      $post: (option: { body: Methods2['post']['reqBody'], query: Methods2['post']['query'], config?: T | undefined }) =>
-        fetch<Methods2['post']['resBody'], BasicHeaders, Methods2['post']['status']>(prefix, PATH1, POST, option).json().then(r => r.body),
-      $path: (option?: { method: 'post'; query: Methods2['post']['query'] } | undefined) =>
-        `${prefix}${PATH1}${option && option.query ? `?${dataToURLString(option.query)}` : ''}`
+      $post: (option: { body: Methods_19v0g2l['post']['reqBody'], query: Methods_19v0g2l['post']['query'], config?: T | undefined }) =>
+        fetch<Methods_19v0g2l['post']['resBody'], BasicHeaders, Methods_19v0g2l['post']['status']>(prefix, PATH1, POST, option).json().then(r => r.body),
+      $path: (option?: { method: 'post'; query: Methods_19v0g2l['post']['query'] } | undefined) =>
+        `${prefix}${PATH1}${option && option.query ? `?${dataToURLString(option.query)}` : ''}`,
     },
     lectures: {
-      _exam_id: (val1: number | string) => {
-        const prefix1 = `${PATH2}/${val1}`
+      _lecture_id: (val1: number | string) => {
+        const prefix1 = `${PATH2}/${val1}`;
 
         return {
-          /**
-           * @returns ok
-           */
-          get: (option?: { config?: T | undefined } | undefined) =>
-            fetch<Methods5['get']['resBody'], BasicHeaders, Methods5['get']['status']>(prefix, prefix1, GET, option).json(),
-          /**
-           * @returns ok
-           */
-          $get: (option?: { config?: T | undefined } | undefined) =>
-            fetch<Methods5['get']['resBody'], BasicHeaders, Methods5['get']['status']>(prefix, prefix1, GET, option).json().then(r => r.body),
-          $path: () => `${prefix}${prefix1}`
-        }
+          exams: {
+            /**
+             * @returns ok
+             */
+            get: (option?: { config?: T | undefined } | undefined) =>
+              fetch<Methods_1ci9g1q['get']['resBody'], BasicHeaders, Methods_1ci9g1q['get']['status']>(prefix, `${prefix1}${PATH0}`, GET, option).json(),
+            /**
+             * @returns ok
+             */
+            $get: (option?: { config?: T | undefined } | undefined) =>
+              fetch<Methods_1ci9g1q['get']['resBody'], BasicHeaders, Methods_1ci9g1q['get']['status']>(prefix, `${prefix1}${PATH0}`, GET, option).json().then(r => r.body),
+            $path: () => `${prefix}${prefix1}${PATH0}`,
+          },
+        };
       },
       search: {
         /**
          * @param option.body - 絞り込み
          * @returns ok
          */
-        post: (option: { body: Methods6['post']['reqBody'], config?: T | undefined }) =>
-          fetch<Methods6['post']['resBody'], BasicHeaders, Methods6['post']['status']>(prefix, PATH3, POST, option).json(),
+        post: (option: { body: Methods_1a3o5fu['post']['reqBody'], config?: T | undefined }) =>
+          fetch<Methods_1a3o5fu['post']['resBody'], BasicHeaders, Methods_1a3o5fu['post']['status']>(prefix, PATH3, POST, option).json(),
         /**
          * @param option.body - 絞り込み
          * @returns ok
          */
-        $post: (option: { body: Methods6['post']['reqBody'], config?: T | undefined }) =>
-          fetch<Methods6['post']['resBody'], BasicHeaders, Methods6['post']['status']>(prefix, PATH3, POST, option).json().then(r => r.body),
-        $path: () => `${prefix}${PATH3}`
+        $post: (option: { body: Methods_1a3o5fu['post']['reqBody'], config?: T | undefined }) =>
+          fetch<Methods_1a3o5fu['post']['resBody'], BasicHeaders, Methods_1a3o5fu['post']['status']>(prefix, PATH3, POST, option).json().then(r => r.body),
+        $path: () => `${prefix}${PATH3}`,
       },
       /**
        * @returns ok
        */
       get: (option?: { config?: T | undefined } | undefined) =>
-        fetch<Methods4['get']['resBody'], BasicHeaders, Methods4['get']['status']>(prefix, PATH2, GET, option).json(),
+        fetch<Methods_iigf21['get']['resBody'], BasicHeaders, Methods_iigf21['get']['status']>(prefix, PATH2, GET, option).json(),
       /**
        * @returns ok
        */
       $get: (option?: { config?: T | undefined } | undefined) =>
-        fetch<Methods4['get']['resBody'], BasicHeaders, Methods4['get']['status']>(prefix, PATH2, GET, option).json().then(r => r.body),
+        fetch<Methods_iigf21['get']['resBody'], BasicHeaders, Methods_iigf21['get']['status']>(prefix, PATH2, GET, option).json().then(r => r.body),
       /**
        * @param option.body - 講義の名前
        */
-      post: (option: { body: Methods4['post']['reqBody'], config?: T | undefined }) =>
-        fetch<void, BasicHeaders, Methods4['post']['status']>(prefix, PATH2, POST, option).send(),
+      post: (option: { body: Methods_iigf21['post']['reqBody'], config?: T | undefined }) =>
+        fetch<void, BasicHeaders, Methods_iigf21['post']['status']>(prefix, PATH2, POST, option).send(),
       /**
        * @param option.body - 講義の名前
        */
-      $post: (option: { body: Methods4['post']['reqBody'], config?: T | undefined }) =>
-        fetch<void, BasicHeaders, Methods4['post']['status']>(prefix, PATH2, POST, option).send().then(r => r.body),
-      $path: () => `${prefix}${PATH2}`
+      $post: (option: { body: Methods_iigf21['post']['reqBody'], config?: T | undefined }) =>
+        fetch<void, BasicHeaders, Methods_iigf21['post']['status']>(prefix, PATH2, POST, option).send().then(r => r.body),
+      $path: () => `${prefix}${PATH2}`,
     },
     login: {
       /**
        * @param option.body - メールアドレスとパスワードを含めたjson
        */
-      post: (option: { body: Methods7['post']['reqBody'], config?: T | undefined }) =>
-        fetch<void, Methods7['post']['resHeaders'], Methods7['post']['status']>(prefix, PATH4, POST, option).send(),
+      post: (option: { body: Methods_idk8rz['post']['reqBody'], config?: T | undefined }) =>
+        fetch<void, Methods_idk8rz['post']['resHeaders'], Methods_idk8rz['post']['status']>(prefix, PATH4, POST, option).send(),
       /**
        * @param option.body - メールアドレスとパスワードを含めたjson
        */
-      $post: (option: { body: Methods7['post']['reqBody'], config?: T | undefined }) =>
-        fetch<void, Methods7['post']['resHeaders'], Methods7['post']['status']>(prefix, PATH4, POST, option).send().then(r => r.body),
-      $path: () => `${prefix}${PATH4}`
+      $post: (option: { body: Methods_idk8rz['post']['reqBody'], config?: T | undefined }) =>
+        fetch<void, Methods_idk8rz['post']['resHeaders'], Methods_idk8rz['post']['status']>(prefix, PATH4, POST, option).send().then(r => r.body),
+      $path: () => `${prefix}${PATH4}`,
     },
     signup: {
       auth: {
@@ -170,56 +182,56 @@ const api = <T>({ baseURL, fetch }: AspidaClient<T>) => {
          * @param option.body - ワンタイムパスワードを含めたjson
          * @returns 認証に成功したら、再びメールアドレスを返す
          */
-        post: (option: { body: Methods8['post']['reqBody'], config?: T | undefined }) =>
-          fetch<Methods8['post']['resBody'], BasicHeaders, Methods8['post']['status']>(prefix, PATH5, POST, option).json(),
+        post: (option: { body: Methods_1fdblld['post']['reqBody'], config?: T | undefined }) =>
+          fetch<Methods_1fdblld['post']['resBody'], BasicHeaders, Methods_1fdblld['post']['status']>(prefix, PATH5, POST, option).json(),
         /**
          * @param option.body - ワンタイムパスワードを含めたjson
          * @returns 認証に成功したら、再びメールアドレスを返す
          */
-        $post: (option: { body: Methods8['post']['reqBody'], config?: T | undefined }) =>
-          fetch<Methods8['post']['resBody'], BasicHeaders, Methods8['post']['status']>(prefix, PATH5, POST, option).json().then(r => r.body),
-        $path: () => `${prefix}${PATH5}`
+        $post: (option: { body: Methods_1fdblld['post']['reqBody'], config?: T | undefined }) =>
+          fetch<Methods_1fdblld['post']['resBody'], BasicHeaders, Methods_1fdblld['post']['status']>(prefix, PATH5, POST, option).json().then(r => r.body),
+        $path: () => `${prefix}${PATH5}`,
       },
       mail: {
         /**
          * @param option.body - メールアドレスを含めたjson
          */
-        post: (option: { body: Methods9['post']['reqBody'], config?: T | undefined }) =>
-          fetch<void, Methods9['post']['resHeaders'], Methods9['post']['status']>(prefix, PATH6, POST, option).send(),
+        post: (option: { body: Methods_jq4fuo['post']['reqBody'], config?: T | undefined }) =>
+          fetch<void, Methods_jq4fuo['post']['resHeaders'], Methods_jq4fuo['post']['status']>(prefix, PATH6, POST, option).send(),
         /**
          * @param option.body - メールアドレスを含めたjson
          */
-        $post: (option: { body: Methods9['post']['reqBody'], config?: T | undefined }) =>
-          fetch<void, Methods9['post']['resHeaders'], Methods9['post']['status']>(prefix, PATH6, POST, option).send().then(r => r.body),
-        $path: () => `${prefix}${PATH6}`
+        $post: (option: { body: Methods_jq4fuo['post']['reqBody'], config?: T | undefined }) =>
+          fetch<void, Methods_jq4fuo['post']['resHeaders'], Methods_jq4fuo['post']['status']>(prefix, PATH6, POST, option).send().then(r => r.body),
+        $path: () => `${prefix}${PATH6}`,
       },
       register: {
         /**
          * @param option.body - メールアドレス、名前、パスワードを含めたjson
          */
-        post: (option: { body: Methods10['post']['reqBody'], config?: T | undefined }) =>
-          fetch<void, Methods10['post']['resHeaders'], Methods10['post']['status']>(prefix, PATH7, POST, option).send(),
+        post: (option: { body: Methods_1izjkiy['post']['reqBody'], config?: T | undefined }) =>
+          fetch<void, Methods_1izjkiy['post']['resHeaders'], Methods_1izjkiy['post']['status']>(prefix, PATH7, POST, option).send(),
         /**
          * @param option.body - メールアドレス、名前、パスワードを含めたjson
          */
-        $post: (option: { body: Methods10['post']['reqBody'], config?: T | undefined }) =>
-          fetch<void, Methods10['post']['resHeaders'], Methods10['post']['status']>(prefix, PATH7, POST, option).send().then(r => r.body),
-        $path: () => `${prefix}${PATH7}`
-      }
+        $post: (option: { body: Methods_1izjkiy['post']['reqBody'], config?: T | undefined }) =>
+          fetch<void, Methods_1izjkiy['post']['resHeaders'], Methods_1izjkiy['post']['status']>(prefix, PATH7, POST, option).send().then(r => r.body),
+        $path: () => `${prefix}${PATH7}`,
+      },
     },
     /**
      * @returns OK
      */
     get: (option?: { config?: T | undefined } | undefined) =>
-      fetch<Methods0['get']['resBody'], BasicHeaders, Methods0['get']['status']>(prefix, '', GET, option).json(),
+      fetch<Methods_by08hd['get']['resBody'], BasicHeaders, Methods_by08hd['get']['status']>(prefix, '', GET, option).json(),
     /**
      * @returns OK
      */
     $get: (option?: { config?: T | undefined } | undefined) =>
-      fetch<Methods0['get']['resBody'], BasicHeaders, Methods0['get']['status']>(prefix, '', GET, option).json().then(r => r.body),
-    $path: () => `${prefix}`
-  }
-}
+      fetch<Methods_by08hd['get']['resBody'], BasicHeaders, Methods_by08hd['get']['status']>(prefix, '', GET, option).json().then(r => r.body),
+    $path: () => `${prefix}`,
+  };
+};
 
-export type ApiInstance = ReturnType<typeof api>
-export default api
+export type ApiInstance = ReturnType<typeof api>;
+export default api;
--- api/lectures/$api.ts
+++ api/lectures/$api.ts
@@ -1,71 +1,74 @@
-import type { AspidaClient, BasicHeaders } from 'aspida'
-import type { Methods as Methods0 } from '.'
-import type { Methods as Methods1 } from './_exam_id'
-import type { Methods as Methods2 } from './search'
+import type { AspidaClient, BasicHeaders } from 'aspida';
+import type { Methods as Methods_by08hd } from '.';
+import type { Methods as Methods_1rmpaye } from './_lecture_id/exams';
+import type { Methods as Methods_4ofto2 } from './search';
 
 const api = <T>({ baseURL, fetch }: AspidaClient<T>) => {
-  const prefix = (baseURL === undefined ? 'http://127.0.0.1:8080' : baseURL).replace(/\/$/, '')
-  const PATH0 = '/lectures'
-  const PATH1 = '/lectures/search'
-  const GET = 'GET'
-  const POST = 'POST'
+  const prefix = (baseURL === undefined ? 'http://127.0.0.1:8080' : baseURL).replace(/\/$/, '');
+  const PATH0 = '/lectures';
+  const PATH1 = '/exams';
+  const PATH2 = '/lectures/search';
+  const GET = 'GET';
+  const POST = 'POST';
 
   return {
-    _exam_id: (val0: number | string) => {
-      const prefix0 = `${PATH0}/${val0}`
+    _lecture_id: (val0: number | string) => {
+      const prefix0 = `${PATH0}/${val0}`;
 
       return {
-        /**
-         * @returns ok
-         */
-        get: (option?: { config?: T | undefined } | undefined) =>
-          fetch<Methods1['get']['resBody'], BasicHeaders, Methods1['get']['status']>(prefix, prefix0, GET, option).json(),
-        /**
-         * @returns ok
-         */
-        $get: (option?: { config?: T | undefined } | undefined) =>
-          fetch<Methods1['get']['resBody'], BasicHeaders, Methods1['get']['status']>(prefix, prefix0, GET, option).json().then(r => r.body),
-        $path: () => `${prefix}${prefix0}`
-      }
+        exams: {
+          /**
+           * @returns ok
+           */
+          get: (option?: { config?: T | undefined } | undefined) =>
+            fetch<Methods_1rmpaye['get']['resBody'], BasicHeaders, Methods_1rmpaye['get']['status']>(prefix, `${prefix0}${PATH1}`, GET, option).json(),
+          /**
+           * @returns ok
+           */
+          $get: (option?: { config?: T | undefined } | undefined) =>
+            fetch<Methods_1rmpaye['get']['resBody'], BasicHeaders, Methods_1rmpaye['get']['status']>(prefix, `${prefix0}${PATH1}`, GET, option).json().then(r => r.body),
+          $path: () => `${prefix}${prefix0}${PATH1}`,
+        },
+      };
     },
     search: {
       /**
        * @param option.body - 絞り込み
        * @returns ok
        */
-      post: (option: { body: Methods2['post']['reqBody'], config?: T | undefined }) =>
-        fetch<Methods2['post']['resBody'], BasicHeaders, Methods2['post']['status']>(prefix, PATH1, POST, option).json(),
+      post: (option: { body: Methods_4ofto2['post']['reqBody'], config?: T | undefined }) =>
+        fetch<Methods_4ofto2['post']['resBody'], BasicHeaders, Methods_4ofto2['post']['status']>(prefix, PATH2, POST, option).json(),
       /**
        * @param option.body - 絞り込み
        * @returns ok
        */
-      $post: (option: { body: Methods2['post']['reqBody'], config?: T | undefined }) =>
-        fetch<Methods2['post']['resBody'], BasicHeaders, Methods2['post']['status']>(prefix, PATH1, POST, option).json().then(r => r.body),
-      $path: () => `${prefix}${PATH1}`
+      $post: (option: { body: Methods_4ofto2['post']['reqBody'], config?: T | undefined }) =>
+        fetch<Methods_4ofto2['post']['resBody'], BasicHeaders, Methods_4ofto2['post']['status']>(prefix, PATH2, POST, option).json().then(r => r.body),
+      $path: () => `${prefix}${PATH2}`,
     },
     /**
      * @returns ok
      */
     get: (option?: { config?: T | undefined } | undefined) =>
-      fetch<Methods0['get']['resBody'], BasicHeaders, Methods0['get']['status']>(prefix, PATH0, GET, option).json(),
+      fetch<Methods_by08hd['get']['resBody'], BasicHeaders, Methods_by08hd['get']['status']>(prefix, PATH0, GET, option).json(),
     /**
      * @returns ok
      */
     $get: (option?: { config?: T | undefined } | undefined) =>
-      fetch<Methods0['get']['resBody'], BasicHeaders, Methods0['get']['status']>(prefix, PATH0, GET, option).json().then(r => r.body),
+      fetch<Methods_by08hd['get']['resBody'], BasicHeaders, Methods_by08hd['get']['status']>(prefix, PATH0, GET, option).json().then(r => r.body),
     /**
      * @param option.body - 講義の名前
      */
-    post: (option: { body: Methods0['post']['reqBody'], config?: T | undefined }) =>
-      fetch<void, BasicHeaders, Methods0['post']['status']>(prefix, PATH0, POST, option).send(),
+    post: (option: { body: Methods_by08hd['post']['reqBody'], config?: T | undefined }) =>
+      fetch<void, BasicHeaders, Methods_by08hd['post']['status']>(prefix, PATH0, POST, option).send(),
     /**
      * @param option.body - 講義の名前
      */
-    $post: (option: { body: Methods0['post']['reqBody'], config?: T | undefined }) =>
-      fetch<void, BasicHeaders, Methods0['post']['status']>(prefix, PATH0, POST, option).send().then(r => r.body),
-    $path: () => `${prefix}${PATH0}`
-  }
-}
+    $post: (option: { body: Methods_by08hd['post']['reqBody'], config?: T | undefined }) =>
+      fetch<void, BasicHeaders, Methods_by08hd['post']['status']>(prefix, PATH0, POST, option).send().then(r => r.body),
+    $path: () => `${prefix}${PATH0}`,
+  };
+};
 
-export type ApiInstance = ReturnType<typeof api>
-export default api
+export type ApiInstance = ReturnType<typeof api>;
+export default api;
