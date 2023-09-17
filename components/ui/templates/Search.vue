e25c6c30a52655271a7b52806492243d60e90c3c
yutteee <yusaku_nakamura_aa@icloud.com>
2023-09-17 21:12:49 +0900

    検索部分のスタイルを実装 #44

--- components/ui/templates/Search.vue
+++ components/ui/templates/Search.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="bg-teal-600 pt-10 pb-10">
-    <v-container>
-      <UiSearchBar
-        v-model="teacherName"
-        title="講義名"
-        placeholder="講義名を入力"
-      />
-      <UiSearchBar
-        v-model="lectureName"
-        title="教授名"
-        placeholder="担当教員名を入力"
-      />
+  <div class="bg-nu-teritary pt-10 pb-10">
+    <v-container
+      class="flex flex-col items-center justify-center gap-8 md:flex-row md:items-end"
+    >
+      <div class="flex w-full max-w-[1000px] flex-col gap-6">
+        <UiSearchBar
+          v-model="teacherName"
+          title="講義名"
+          placeholder="講義名を入力"
+        />
+        <UiSearchBar
+          v-model="lectureName"
+          title="教授名"
+          placeholder="担当教員名を入力"
+        />
+      </div>
+      <v-btn
+        @click="emits('click', teacherName, lectureName)"
+        append-icon="mdi-magnify"
+        color="#006E4F"
+        class="w-fit text-white"
+        >検索</v-btn
+      >
     </v-container>
-    <v-btn @click="emits('click', teacherName, lectureName)">name</v-btn>
   </div>
 </template>
 
