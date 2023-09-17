12726115d3ecf84844ee2ab0c4469be4fb6d715e
yutteee <yusaku_nakamura_aa@icloud.com>
2023-09-17 20:29:53 +0900

    teacherNameとlectureNameを制御できる様にした #44

--- components/ui/SearchBar.vue
+++ components/ui/SearchBar.vue
@@ -1,25 +1,25 @@
 <template>
-  <v-text-field
-    density="compact"
-    variant="solo"
-    label="講義名、教授名で講義を検索"
-    append-inner-icon="mdi-magnify"
-    single-line
-    hide-details
-    @click:append-inner="onClick"
-    v-model="text"
-  ></v-text-field>
+  <div class="flex flex-row items-center gap-8">
+    <div class="text-white">
+      {{ title }}
+    </div>
+    <v-text-field
+      density="compact"
+      variant="solo"
+      single-line
+      hide-details
+      :label="placeholder"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
-  interface Emits {
-    (e: "click", text: string | undefined): void;
+  interface Props {
+    title: string;
+    placeholder: string;
+    modelValue: string | undefined;
   }
-  const emit = defineEmits<Emits>();
-
-  const text = ref<string | undefined>();
-
-  const onClick = function () {
-    emit("click", text.value);
-  };
+  const props = defineProps<Props>();
 </script>
