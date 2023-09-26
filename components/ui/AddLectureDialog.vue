ae2084103486259397dd0302fc3c944cd986f2cb
nac-39 <83295760+nac-39@users.noreply.github.com>
2023-09-26 05:55:40 +0000

    update: 講義追加もバックエンドに合わせた

--- components/ui/AddLectureDialog.vue
+++ components/ui/AddLectureDialog.vue
@@ -8,13 +8,6 @@
       <v-card class="w-96">
         <v-card-title class="headline text-primary">講義追加</v-card-title>
         <v-col>
-          <v-text-field
-            label="年度"
-            v-model.number="year"
-            prepend-icon="mdi-calendar-blank"
-          ></v-text-field>
-        </v-col>
-        <v-col>
           <UiSelect
             label="学年"
             :items="grades"
@@ -68,41 +61,23 @@
 <script setup lang="ts">
   import { GradeType } from "~~/types/grade";
   import { TermType } from "~~/types/term";
-  import { faculties } from "~~/entities/faculties";
   import { terms } from "~~/entities/terms";
   import { grades } from "~~/entities/lecture";
   import { useClient } from "~~/util/api/useApi";
   const props = defineProps<{
     dialog: boolean;
     departmentId?: number;
-    year?: number;
   }>();
   const emit = defineEmits<{
     (event: "update:dialog", value: boolean): void;
     (event: "update:departmentId", value: number): void;
-    (event: "update:year", value: number): void;
   }>();
   const isShow = computed({
     get: () => props.dialog,
     set: (value) => emit("update:dialog", value)
   });
-  // OPTION
-  const facultiesOption = faculties.map((value, index) => {
-    return {
-      id: index,
-      name: value
-    };
-  });
 
   // POSTするデータ
-  const year = computed({
-    get: () => props.year,
-    set: (value) => emit("update:year", value)
-  });
-  const departmentId = computed({
-    get: () => props.departmentId,
-    set: (value) => emit("update:departmentId", value)
-  });
   const term = ref<TermType>();
   const name = ref("");
   const teacherName = ref("");
@@ -113,10 +88,9 @@
     const client = useClient();
     client.lectures.$post({
       body: {
-        year: year.value,
         term: term.value,
-        name: name.value,
         teacherName: teacherName.value,
+        lectureName: name.value,
         grade: grade.value
       }
     });
