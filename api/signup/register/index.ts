a4c03f5cf09c767d4e1c092c222d47f287d696d1
hirakuuuu <m.hiraku1129@gmail.com>
2023-10-22 22:37:32 +0900

    :+1: 本登録時にメールアドレスを送らないように修正 #51

--- api/signup/register/index.ts
+++ api/signup/register/index.ts
@@ -1,17 +1,16 @@
 /* eslint-disable */
 export type Methods = {
   post: {
-    status: 200;
+    status: 200
 
     resHeaders: {
-      "Set-Cookie": string;
-    };
+      'Set-Cookie': string
+    }
 
-    /** メールアドレス、名前、パスワードを含めたjson */
+    /** 名前、パスワードを含めたjson */
     reqBody: {
-      email: string;
-      name: string;
-      password: string;
-    };
-  };
-};
+      name: string
+      password: string
+    }
+  }
+}
