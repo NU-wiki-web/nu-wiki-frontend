2d647841d29039b510ce990e7750acb21eb39820
nac-39 <83295760+nac-39@users.noreply.github.com>
2023-11-28 13:42:56 +0000

    モーダルにexamを表示するようにした #58

--- components/pages/lecture/lectureSelectModal.vue
+++ components/pages/lecture/lectureSelectModal.vue
@@ -1,36 +1,39 @@
 <template>
-  <div class="fixed inset-0 z-10 overflow-y-auto">
+  <div class="fixed inset-0 z-10 h-screen">
     <div
       class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
       @click.self="$emit('close')"
     >
       <div
-        class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-3xl"
+        class="relative transform rounded-lg bg-white shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-3xl"
       >
         <div class="bg-nu-quaternary px-5 pb-4 pt-5 sm:p-6 sm:pb-4">
           <div class="sm:flex sm:items-start">
-            <h3 class="text-2xl font-semibold leading-6 text-gray-900"></h3>
+            <h3 class="text-2xl font-semibold leading-6 text-gray-900">
+              {{ lecture.lectureName + "のテスト一覧" }}
+            </h3>
           </div>
         </div>
-        <div class="p-5 text-lg">
-          <p>
-            <v-icon icon="mdi-account" />
-          </p>
-          <p>
-            <v-icon icon="mdi-calendar-blank" />
-            作成日
-          </p>
-          <p>
-            <v-icon icon="mdi-calendar-badge" />
-            更新日
-          </p>
-        </div>
-        <div class="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-          <button
-            class="mb-2 mr-2 rounded-lg bg-nu-primary px-5 py-2.5 text-sm font-bold text-white focus:outline-none focus:ring-4 focus:ring-blue-300"
+        <div
+          v-if="lecture.exams && lecture.exams?.length > 0"
+          class="mx-auto flex flex-wrap justify-center overflow-y-auto"
+        >
+          <div
+            v-for="exam in lecture.exams"
+            class="m-2 flex h-16 w-2/5 cursor-pointer rounded-md shadow-md hover:shadow-lg"
           >
-            ファイルを開く
-          </button>
+            <div class="flex w-1/2 items-center justify-center rounded-l-md">
+              {{ exam.year }}
+            </div>
+            <div
+              class="flex w-1/2 items-center justify-center rounded-r-md bg-nu-primary text-white"
+            >
+              {{ examTypeJa(exam.type as PastExamType) }}
+            </div>
+          </div>
+        </div>
+        <div v-else class="mx-auto flex justify-center p-16">
+          テストがありません
         </div>
       </div>
     </div>
@@ -38,12 +41,15 @@
 </template>
 
 <script setup lang="ts">
+  import { Lecture } from "~~/api/@types";
+  import { PastExamType, usePastExamType } from "~~/entities/pastExam";
   import { PdfType } from "~~/types/pdf";
 
-  defineProps(["pdf"]);
   interface Props {
     pdf: PdfType;
+    lecture: Lecture;
   }
+  defineProps<Props>();
 
   const formatDate = (date: string) => {
     let [y, m, d] = date.split("-");
@@ -59,4 +65,6 @@
     let url = getUrl(name);
     window.open(url, "_blank");
   };
+
+  const examTypeJa = usePastExamType().convertEn2Ja;
 </script>
