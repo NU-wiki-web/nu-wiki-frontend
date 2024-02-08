e3aa4c58bfd247b743f503c6b83a25534814ad15
nac-39 <83295760+nac-39@users.noreply.github.com>
2024-02-08 20:36:35 +0900

    [add] pdf一覧ページで講義名と試験名を見れるようにした

--- components/pages/pdf-list/index.vue
+++ components/pages/pdf-list/index.vue
@@ -1,9 +1,18 @@
 <template>
   <UiHeader></UiHeader>
   <div v-if="total">
+    <div class="bg-nu-teritary p-8 text-white">
+      <h1 class="text-2xl">
+        <span class="font-bold">
+          "{{ lecture?.lectureName }}({{ lecture?.teacherName }}先生)
+          {{ exam?.year }}年度{{ termType2ja(exam?.type!) }}"
+        </span>
+        のPDF一覧
+      </h1>
+    </div>
     <v-list v-for="pdf in pdfs" :key="pdf.id" class="mx-10 my-5">
       <v-list-item
-        :title="type2ja(pdf.type ? pdf.type : 'other')"
+        :title="pdf.name ?? '' + type2ja(pdf.type ? pdf.type : 'other')"
         link
         border
         @click="showDetailModal(pdf.id)"
@@ -33,15 +42,21 @@
 <script setup lang="ts">
   import pdfDetailModal from "./pdfDetailModal.vue";
   import { useClient } from "~/util/api/useApi";
-  import { Pdf as PdfType } from "~~/api/@types";
-  import { useFileType } from "~~/entities/pastExam";
+  import { Exam, Lecture, Pdf as PdfType } from "~~/api/@types";
+  import { useFileType, usePastExamType } from "~~/entities/pastExam";
 
   const client = useClient();
 
   // クエリパラメータの取得
   const route = useRoute();
+  const lecture_id =
+    typeof route.params.lecture_id == "string"
+      ? route.params.lecture_id
+      : route.params.lecture_id[0];
   const exam_id =
-    typeof route.params.id == "string" ? route.params.id : route.params.id[0];
+    typeof route.params.exam_id == "string"
+      ? route.params.exam_id
+      : route.params.exam_id[1];
 
   // pdf情報の取得
   const total = ref<number>(0);
@@ -51,8 +66,16 @@
   const fileObjectURL = ref<string>();
   const showDetail = ref<boolean>();
   const showingPdfId = ref<string>();
+  const lecture = ref<Lecture>();
+  const exam = ref<Exam>();
 
-  console.log(exam_id);
+  client.lectures.$get().then((res) => {
+    // NOTE: GET /lectures/:lecture_idってなんでないんだっけ？
+    lecture.value = res.lectures.find((l) => l.id === lecture_id);
+    // NOTE: GET /exams/:exam_idってなんでないんだっけ？
+    exam.value = lecture.value?.exams?.find((e) => e.id === exam_id);
+  });
+
   client.exams
     ._exam_id(exam_id)
     .files.$get()
@@ -69,6 +92,7 @@
 
   /* pdfの詳細表示 */
   const type2ja = useFileType().convertEn2Ja;
+  const termType2ja = usePastExamType().convertEn2Ja;
   const showDetailModal = (fileId: string | undefined) => {
     if (fileId === undefined) return;
     client.files
