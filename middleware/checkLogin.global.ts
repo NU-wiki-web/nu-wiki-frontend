937c243dc0d55df728a9382e02a6774899fc08f3
nac-39 <83295760+nac-39@users.noreply.github.com>
2024-03-27 14:49:37 +0900

    feat: middlewareでcookie使えるぞ！！！ログインしてないときの処理をついかしました　！

--- middleware/checkLogin.global.ts
+++ middleware/checkLogin.global.ts
@@ -1,39 +1,23 @@
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware((to, _) => {
+  // クライアントサイドでは実行しない。
+  if (process.client) return;
   const sessionId = useCookie("SESSIONID");
-  console.log("middleware: sessionId", sessionId.value);
-  console.log("middleware: to.path", to.path);
-  //   if (to.path === "/login" || to.path === "/") {
-  //     return true;
-  //   }
-  //   if (!sessionId.value) {
-  //     return navigateTo("/login");
-  //   }
-  // }
-  const signupId = useCookie("SIGNUPID");
   const pathsAllowNonLogin = ["/login", "/", "/signup"];
   const signUpPaths = ["/signup/auth", "/signup/register"];
 
-  // if (to.path == "/login" && sessionId.value) {
-  //   console.log("middleware: redirect to /lecture-list");
-  //   return navigateTo("/lecture-list");
-  // }
-  // ログイン無しで見れるページは全て許可する
-  if (pathsAllowNonLogin.includes(to.path)) {
-    return true;
-  }
-
-  // ユーザー登録の途中なら許可
-  if (from.path == "/signup" && to.path == "/signup/auth") {
-    return true;
-  } else if (from.path == "/signup/auth" && to.path == "/signup/register") {
-    return true;
-  }
-
-  // ログインしていない場合はログイン画面へリダイレクト
-  if (!sessionId.value) {
-    console.log("middleware: redirect to /login");
-    return navigateTo("/login");
+  if (sessionId.value) {
+    // ログイン済みのとき
+    if (to.path === "/login" || signUpPaths.includes(to.path)) {
+      return navigateTo("/lecture-list");
+    }
+    return;
   } else {
-    return true;
+    if (pathsAllowNonLogin.includes(to.path) || signUpPaths.includes(to.path)) {
+      // ログイン無しで見れるページなら許可
+      return;
+    } else {
+      // それ以外はログインページにリダイレクト
+      return navigateTo("/login");
+    }
   }
 });
